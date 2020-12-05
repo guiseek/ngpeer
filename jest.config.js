305e12c135d71a55f42f3ab065278f1fb1f61bea
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-12-04 23:09:57 -0300

    chore: sample

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/libs/core', '<rootDir>/libs/client'],
+  projects: [
+    '<rootDir>/libs/core',
+    '<rootDir>/libs/client',
+    '<rootDir>/apps/client/group',
+  ],
 }
