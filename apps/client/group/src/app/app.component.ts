fe088c54043200a128811c07d1867ab9e4981f08
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-12-05 00:39:21 -0300

    feat: material design

--- apps/client/group/src/app/app.component.ts
+++ apps/client/group/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { PeerClient } from '@ngpeer/core'
 import { AfterViewInit, Component } from '@angular/core'
+import { BreakpointObserver } from '@angular/cdk/layout'
 import { ClientConnectionService, ClientStoreService } from '@ngpeer/client'
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'
 
@@ -12,20 +13,20 @@ export class AppComponent implements AfterViewInit {
   title = 'client-group'
 
   public peerClients: PeerClient[]
-
-  photos = [
-    'assets/photo1.png',
-    'assets/photo2.png',
-    'assets/photo3.png',
-  ]
-
+  isSmallScreen = this.breakpointObserver.isMatched('(max-width: 599px)');
+  breakpoints$ = this.breakpointObserver.observe('')
   constructor(
-    private clientStoreService: ClientStoreService,
     private connectionService: ClientConnectionService,
+    private clientStoreService: ClientStoreService,
+    private breakpointObserver: BreakpointObserver,
     private sanitizer: DomSanitizer
   ) {
     this.clientStoreService.clients$.subscribe(
-      (clientList) => (this.peerClients = clientList.toArray()),
+      (clientList) => {
+        console.log(clientList);
+
+        this.peerClients = clientList.toArray()
+      },
       (err) => console.error('Error updating the client list:', err)
     )
   }
@@ -35,8 +36,10 @@ export class AppComponent implements AfterViewInit {
       URL.createObjectURL(stream)
     )
   }
-
+  connect() {
+    this.connectionService.connectToRoom()
+  }
   ngAfterViewInit(): void {
-    // this.connectionService.connectToRoom()
+
   }
 }
--- apps/client/group/src/app/app.module.ts
+++ apps/client/group/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core'
 import { ClientModule } from '@ngpeer/client'
 
 import { AppComponent } from './app.component'
-import { RouterModule } from '@angular/router'
+import { RouterModule } from '@angular/router';
+import { LayoutModule } from '@angular/cdk/layout';
+import { PlatformModule } from '@angular/cdk/platform';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    LayoutModule,
     BrowserModule,
+    MatIconModule,
+    PlatformModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatGridListModule,
     ClientModule.forRoot('http://localhost:3000'),
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
