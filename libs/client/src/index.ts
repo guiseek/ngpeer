53a6f6551c8ee26cc559a861f28ed4b52a3047d1
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-12-05 12:00:47 -0300

    chore: demo server

--- libs/client/src/index.ts
+++ libs/client/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/client.module'
+export { ClientStoreService } from './lib/client-store.service'
+export { MediaStreamService } from './lib/media-stream.service'
+export { ClientConnectionService, CONFIG } from './lib/client-connection.service'
