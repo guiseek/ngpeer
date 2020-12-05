375b1413e6634933ae84cbb42933450b6f0adb93
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-12-04 21:42:16 -0300

    feat: ngpeer/core peer actions and client

---
+++ libs/core/src/lib/peer-action.enum.ts
@@ -0,0 +1,9 @@
+export enum PeerAction {
+  Ice = '[Peer] Ice',
+  Data = '[Peer] Data',
+  Offer = '[Peer] offer',
+  Answer = '[Peer] answer',
+  Connected = '[Peer] Connected',
+  Disconnected = '[Peer] Disconnected',
+  ConnectToRoom = '[Peer] Connect to room',
+}
